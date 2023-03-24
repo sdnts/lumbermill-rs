95283d7f0f760e204f7e4d9b7390068b1fc10807
Siddhant <siddhant@fastmail.com>
2023-03-24 07:15:35 +0100

    feat(logfmt): log to stdout

--- crates/logfmt/src/logger.rs
+++ crates/logfmt/src/logger.rs
@@ -1,8 +1,16 @@
+use std::io::{stdout, Write};
+
 pub struct Logger;
 pub static LOGGER: Logger = Logger;
 
 impl Logger {
-  pub fn log(&self, kv: Vec<(String, String)>) {
-    _ = kv;
+  pub fn log(&mut self, kv: Vec<(&'static str, String)>) {
+    let mut stdout = stdout();
+
+    kv.iter().for_each(|(k, v)| {
+      _ = write!(stdout, "{}=\"{}\" ", k, v);
+    });
+
+    _ = writeln!(stdout);
   }
 }
