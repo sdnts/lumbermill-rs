ced2b41409c9114f5b2da0dd85050163620845aa
Siddhant <siddhant@fastmail.com>
2023-03-24 20:16:12 +0100

    feat(logfmt): log to file

--- crates/logfmt/src/logger.rs
+++ crates/logfmt/src/logger.rs
@@ -1,7 +1,30 @@
-use std::io::{stdout, Write};
+use once_cell::sync::Lazy;
+use parking_lot::Mutex;
+use std::{
+  fs::{self, File},
+  io::{stdout, Write},
+};
 
-pub struct Logger;
-pub static LOGGER: Logger = Logger;
+pub struct Logger {
+  // format: "pretty" | "compact"
+  // common_fields
+  file: File,
+}
+pub static LOGGER: Lazy<Mutex<Logger>> = Lazy::new(|| {
+  #[cfg(debug_assertions)]
+  let dir = "logs";
+  #[cfg(not(debug_assertions))]
+  let dir = "/var/log/toph";
+
+  fs::create_dir_all(dir).expect("Could not create log directory");
+  let file = File::options()
+    .append(true)
+    .create(true)
+    .open(format!("{}/{}", dir, "node.log"))
+    .expect("Could not open file");
+
+  Mutex::new(Logger { file })
+});
 
 impl Logger {
   pub fn log(&mut self, kv: Vec<(&'static str, String)>) {
@@ -9,8 +32,10 @@ impl Logger {
 
     kv.iter().for_each(|(k, v)| {
       _ = write!(stdout, "{}=\"{}\" ", k, v);
+      _ = write!(self.file, "{}=\"{}\" ", k, v);
     });
 
     _ = writeln!(stdout);
+    _ = writeln!(self.file);
   }
 }
